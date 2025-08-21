cb0de8140804cff6e16f2f6903316b788e44ae48
ivan <ivankaweesa.biz@gmail.com>
2025-08-21 02:29:08 +0200

    setup the url patterns to follow for user registration

--- stock_pal/stock_pal/urls.py
+++ stock_pal/stock_pal/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+base_url = 'api/v1'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(f'{base_url}/users/', include('users.urls')),
 ]
