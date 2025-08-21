05e00f940ae776b58860f3647d29476b523fc500
ivan <ivankaweesa.biz@gmail.com>
2025-08-21 02:09:02 +0200

    creted model for user authentication and utilised generic views from rest_framework

--- stock_pal/users/views.py
+++ stock_pal/users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializer import UserRegistrationSerializer
 
-# Create your views here.
+User = get_user_model()
+
+'''
+views
+- handle user registration
+- expect email and password - required
+- validate email and password
+- save the data
+'''
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User_serializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'detail': 'Registration successfull'})
